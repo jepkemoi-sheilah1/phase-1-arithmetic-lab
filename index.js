0e5841904adfd01ceb665aef31b4351c8cf4240e
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-06 15:32:47 +0300

    adds Arithmetic operation to the la

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// declare two variables num1 and num2
+const num1 = 31
+const num2 = 2
+// multiply num1 and num2
+const multiply = num1 * num2//2*31=62
+// generate a random number
+const random = Math.floor(Math.random() * 10) + 1
+// declare two variables num3 and num4
+const num3 = 14
+const num4 = 5
+// calculate the remainder of num3 divided by num4
+const mod = num3 % num4//14%5=4
+// declare an array of numbers
+const max = Math.max(10, 15 , 20, 5)// highest number in the array is 20 this ensure max is equal to 20
+// export the variables
+module.exports = {num1, num2, multiply, random, num3, num4, mod, max};
